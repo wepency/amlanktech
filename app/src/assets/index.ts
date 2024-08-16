b9388d3a7398ad1cb2ced92ccc1a7bae27082dba
X_V.neer <xv.neer@gmail.com>
2024-08-16 11:51:29 +0300

    refactor: change icons and content

--- app/src/assets/index.ts
+++ app/src/assets/index.ts
@@ -1,8 +1,27 @@
+import advantage from "./advantage.svg"
+import building from "./building.svg"
+import contract from "./contract.svg"
+import conversation from "./conversation.svg"
 import image1 from "./image-1.webp"
 import image2 from "./image-2.webp"
 import image3 from "./image-3.webp"
 import logo from "./logo.png"
+import management from "./management.svg"
 import partner1 from "./partner1.jpeg"
 import partner2 from "./partner2.jpg"
+import work from "./work.svg"
 
-export { logo, image1, image2, image3, partner1, partner2 }
+export {
+  logo,
+  image1,
+  image2,
+  image3,
+  partner1,
+  partner2,
+  advantage,
+  building,
+  contract,
+  management,
+  work,
+  conversation,
+}
