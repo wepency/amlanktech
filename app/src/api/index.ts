a239ed96fc0d2a5de880d63a1f4da6a129f17618
MacBook <macbook@MacBook-Pro-2.local>
2024-11-09 23:25:51 +0300

    baseurl changed to production

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 import { getSession } from "next-auth/react"
 
-// const baseURL = "https://dash.amlaktech.com/api"
-const baseURL = "http://127.0.0.1:8000/api"
+const baseURL = "https://dash.amlaktech.com/api"
+// const baseURL = "http://127.0.0.1:8000/api"
 
 const AmlackApi = axios.create({
   baseURL: baseURL,
